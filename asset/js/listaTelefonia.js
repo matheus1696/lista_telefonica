404ba8ccf8dce3a549dc65ea5376871edda4c30e
Matheus André Bezerra Mota <113203771+matheus1696@users.noreply.github.com>
2023-08-18 11:23:48 -0300

    Lista Telefônica - Unidade da Atenção Especializada de Caruaru

---
+++ asset/js/listaTelefonia.js
@@ -0,0 +1,123 @@
+const telefonia = [
+    {
+      no_unidade: 'Secretaria de Saúde de Caruaru',
+      ft_unidade: 'secretaria de saúde de caruaru',
+      telefone: '81 3101-0201'
+    },
+    {
+      no_unidade: 'UPA Boa Vista',
+      ft_unidade: 'upa boa vista',
+      telefone: '81 3101-0256'
+    },
+    {
+      no_unidade: 'UPA Rendeiras',
+      ft_unidade: 'upa rendeiras',
+      telefone: '81 3101-0276'
+    },
+    {
+      no_unidade: 'UPA Vassoural',
+      ft_unidade: 'upa vassoural',
+      telefone: '81 3101-0286'
+    },
+    {
+      no_unidade: 'Hospital Manoel Afonso',
+      ft_unidade: 'hospital manoel afonso',
+      telefone: '81 3101-0306'
+    },
+    {
+      no_unidade: 'Maternidade Municipal Santa Dulce dos Pobres',
+      ft_unidade: 'maternidade municipal santa dulce dos pobres',
+      telefone: '81 3101-0326'
+    },
+    {
+      no_unidade: 'Regulação Municipal',
+      ft_unidade: 'regulação municipal',
+      telefone: '81 3101-0346'
+    },
+    {
+      no_unidade: 'AME Caiuca',
+      ft_unidade: 'ame caiuca',
+      telefone: '81 3101-0351'
+    },
+    {
+      no_unidade: 'AME Infanto Juvenil',
+      ft_unidade: 'ame infanto juvenil',
+      telefone: '81 3101-0354'
+    },
+    {
+      no_unidade: 'AME Diagnostico',
+      ft_unidade: 'ame diagnostico',
+      telefone: '81 3101-0356'
+    },
+    {
+      no_unidade: 'AME Indianópolis',
+      ft_unidade: 'ame indianopolis',
+      telefone: '81 3101-0358'
+    },
+    {
+      no_unidade: 'AME Mulher',
+      ft_unidade: 'ame mulher',
+      telefone: '81 3101-0360'
+    },
+    {
+      no_unidade: 'AME Saúde Auditiva',
+      ft_unidade: 'ame Saúde Auditiva',
+      telefone: '81 3101-0362'
+    },
+    {
+      no_unidade: 'Unidade de Atenção Mental',
+      ft_unidade: 'unidade de atenção mental',
+      telefone: '81 3101-0364'
+    },
+    {
+      no_unidade: 'AME Idoso',
+      ft_unidade: 'ame idoso',
+      telefone: '81 3101-0366'
+    },
+    {
+      no_unidade: 'CAPS III AD',
+      ft_unidade: 'caps ad',
+      telefone: '81 3101-0368'
+    },
+    {
+      no_unidade: 'CAPS',
+      ft_unidade: 'caps',
+      telefone: '81 3101-0369'
+    },
+    {
+      no_unidade: 'Unidade de Fisioterapia',
+      ft_unidade: 'unidade de fisioterapia',
+      telefone: '81 3101-0370'
+    },
+    {
+      no_unidade: 'Lacian',
+      ft_unidade: 'lacian',
+      telefone: '81 3101-0371'
+    },
+    {
+      no_unidade: 'Laboratório Central',
+      ft_unidade: 'laboratório central',
+      telefone: '81 3101-0372'
+    },
+    {
+      no_unidade: 'CTA/COAS',
+      ft_unidade: 'cta/coas',
+      telefone: '81 3101-0373'
+    },
+    {
+      no_unidade: 'CEREST',
+      ft_unidade: 'cerest',
+      telefone: '81 3101-0374'
+    },
+    {
+      no_unidade: 'CEO - Centro de Especialidades Odontológicas',
+      ft_unidade: 'ceo - centro de especialidades odontológicas',
+      telefone: '81 3101-0375'
+    },
+    {
+      no_unidade: 'Farmácia São Francisco',
+      ft_unidade: 'farmácia são francisco',
+      telefone: '81 3101-0376'
+    },
+];
+
---
+++ asset/js/search.js
@@ -0,0 +1,29 @@
+for (let i = 0; i < telefonia.length; i++) {
+    console.log(telefonia[i])
+
+    tbody = document.querySelector('tbody');
+    tbody.innerHTML +=`<tr class="list"><td>${telefonia[i].no_unidade}</td><td>${telefonia[i].telefone}</td></tr>`
+}
+
+function search() {
+    let search = document.getElementById('input')
+    search = search.value.toLowerCase()
+
+    let listItems = document.getElementsByClassName('list');
+
+    for (let i = 0; i < listItems.length; i++) {
+        let listItem = listItems[i];
+        let unidade = telefonia[i].ft_unidade.toLowerCase();
+        let posicao = unidade.indexOf(search);        
+
+        if (search !== "") {
+            if (posicao !== -1) {
+                listItem.style.display = "";
+            } else {
+                listItem.style.display = "none";
+            }
+        } else {            
+            listItem.style.display = "";
+        }
+    }
+}
